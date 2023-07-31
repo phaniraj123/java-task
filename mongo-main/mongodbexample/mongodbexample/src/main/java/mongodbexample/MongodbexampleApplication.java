1a0fc4b5f56d9fc3f3a3839862aeed7f8796897e
phaniraj123 <140390293+phaniraj123@users.noreply.github.com>
2023-07-31 23:14:22 +0530

    Add files via upload

---
+++ mongo-main/mongodbexample/mongodbexample/src/main/java/mongodbexample/MongodbexampleApplication.java
@@ -0,0 +1,21 @@
+package mongodbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+@Configuration
+
+
+public class MongodbexampleApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbexampleApplication.class, args);
+	}
+
+}
+
+
